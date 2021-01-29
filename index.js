c5159a6ad29f3824198ab2e096e2739e86f37586
Anton Lazarev <an.lazarev00@gmail.com>
2021-01-29 15:04:50 +0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const Discord = require("discord.js");
+const config = require("./config.json");
+
+const client = new Discord.Client();
+
+const prefix = "#";
+
+client.on("message", function(message) {
+    if (message.author.bot) return;
+    if (!message.content.startsWith(prefix)) return;
+	if (message.channel.name !== 'позитивные-киноманы') return;
+    const commandBody = message.content.slice(prefix.length);
+    const args = commandBody.split(' ');
+    const command = args.shift().toLowerCase();
+
+    if (command === "просмотренныефильмы") {
+        message.react('1⃣');
+        message.react('2⃣');
+        message.react('3⃣');
+        message.react('4⃣');
+        message.react('5⃣');
+    }
+});
+
+client.login(config.BOT_TOKEN);
