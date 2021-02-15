4984cac8efd04774ac8bd90ab0354edbec95a1ba
Anton Lazarev <an.lazarev00@gmail.com>
2021-02-15 20:56:26 +0300

    Add reacts for books

--- index.js
+++ index.js
@@ -10,17 +10,31 @@ const prefix = "#";
 client.on("message", function(message) {
     if (message.author.bot) return;
     if (!message.content.startsWith(prefix)) return;
-	if (message.channel.name !== 'позитивные-киноманы') return;
-    const commandBody = message.content.slice(prefix.length);
-    const args = commandBody.split(/\s+/);
-    const command = args.shift().toLowerCase();
 
-    if (command === "просмотренныефильмы") {
-        message.react('1⃣');
-        message.react('2⃣');
-        message.react('3⃣');
-        message.react('4⃣');
-        message.react('5⃣');
+	if (message.channel.name === 'кинолюб') {
+        const commandBody = message.content.slice(prefix.length);
+        const args = commandBody.split(/\s+/);
+        const command = args.shift().toLowerCase();
+        if (command === "просмотренныефильмы") {
+            message.react('1⃣');
+            message.react('2⃣');
+            message.react('3⃣');
+            message.react('4⃣');
+            message.react('5⃣');
+        }
+    }
+
+    else if (message.channel.name === 'библиотека') {
+        const commandBody = message.content.slice(prefix.length);
+        const args = commandBody.split(/\s+/);
+        const command = args.shift().toLowerCase();
+        if (command === "прочитанныекниги") {
+            message.react('1⃣');
+            message.react('2⃣');
+            message.react('3⃣');
+            message.react('4⃣');
+            message.react('5⃣');
+        }
     }
 });
 
